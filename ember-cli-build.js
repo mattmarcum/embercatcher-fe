a0e422dfcd8c60a2f2c5ad16f502854420717046
Matthew Marcum <matt@strangelooper.com>
2020-02-08 18:21:00 -0600

    first pass init with tailwind and purgecss

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,24 +1,40 @@
 'use strict';
-
+const { join } = require('path');
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const isProduction = true;//EmberApp.env() === 'production';
+
+const purgeCSS = {
+  module: require('@fullhuman/postcss-purgecss'),
+  options: {
+    content: [
+      // add extra paths here for components/controllers which include tailwind classes
+      join(__dirname, 'app', '**', '*.html'),
+      join(__dirname, 'app', '**', '*.hbs')
+    ],
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  }
+}
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    'ember-power-select': {
+      theme: false
+    },
+    postcssOptions: {
+      compile: {
+        plugins: [
+          {
+            module: require('postcss-import'),
+            options: {
+              path: ['node_modules']
+            }
+          },
+          require('tailwindcss')('./config/tailwind.config.js'),
+          ...isProduction ? [purgeCSS] : []
+        ]
+      }
+    }
   });
-
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
 
   return app.toTree();
 };
